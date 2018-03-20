b7c15b5c79dbcbc33a5711324071e4625c425c79
Hendrik Schultze <hendrik.schultze@innogames.com>
2018-03-20 16:37:44 +0100

    removed redundant scss packing

--- app/controllers/FrontController.scala
+++ app/controllers/FrontController.scala
@@ -8,12 +8,10 @@ import play.api.mvc._
 
 class FrontController @Inject() (cc: ControllerComponents, env: Environment, config: Configuration) extends AbstractController(cc){
   def index = Action {
-    //Ok(views.html.index.render(env, config.get[Int]("webpack.port"), WebpackBuildFile.jsBundle, WebpackBuildFile.cssBundle))
-    Ok(views.html.index.render(env, config.get[Int]("webpack.port"), WebpackBuildFile.jsBundle, " "))
+    Ok(views.html.index.render(env, config.get[Int]("webpack.port"), WebpackBuildFile.jsBundle))
   }
 
   def indexWithRoute(x: String) = Action {
-    //Ok(views.html.index.render(env, config.get[Int]("webpack.port"), WebpackBuildFile.jsBundle, WebpackBuildFile.cssBundle))
-    Ok(views.html.index.render(env, config.get[Int]("webpack.port"), WebpackBuildFile.jsBundle, " "))
+    Ok(views.html.index.render(env, config.get[Int]("webpack.port"), WebpackBuildFile.jsBundle))
   }
 }
--- app/lib/WebpackBuildFile.scala
+++ app/lib/WebpackBuildFile.scala
@@ -9,9 +9,4 @@ object WebpackBuildFile {
   val jsBundle: String = if(d.exists && d.isDirectory) {
     d.listFiles.filter(_.isFile).toList.find(f => f.getName.contains("js.bundle.")).get.getName.replace(".gz", "")
   } else ""
-
-//  val cssBundle: String = if(d.exists && d.isDirectory) {
-//    d.listFiles.filter(_.isFile).toList.find(f => f.getName.contains("style.bundle.")).get.getName.replace(".gz", "")
-//  } else ""
-
 }
